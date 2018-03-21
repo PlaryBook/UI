702dc3d3fc1158a24af6b77cf6842a6bde3090a2
Xan42 <ian.johnston@telus.net>
2018-03-21 09:44:44 -0600

    I added a fade out feature for the badge info screen. I added horizontal scroll bar functionality for each of the different lists (reading, wish, purchase history) for when there are more books. It doesn't look as good in blend/VS as it does when it's running. You'll also need to scroll using the arrow keys. I also did some general reorganizing of what assets went in which containers.

--- Plary/MainWindow.xaml.cs
+++ Plary/MainWindow.xaml.cs
@@ -52,8 +52,9 @@ namespace Plary
             this.CreatedContinueButton.Click += OnCreatedContinue;
 
             //Profile Screen (And badge info) functionality
-            FullBadge1.MouseLeftButtonDown += OnFullBadgeClick;
-            BadgeInfoScreen.MouseLeftButtonDown += OnFullBadgeScreenClick;
+            this.ReadingListUniformGrid.Width = this.ReadingListUniformGrid.Children.Count * 180;
+            this.WishListUniformGrid.Width = this.WishListUniformGrid.Children.Count * 180;
+            this.PurchaseHistoryUniformGrid.Width = this.PurchaseHistoryUniformGrid.Children.Count * 180;
         }
 
         #region Login/Create account screen button functionality
@@ -104,24 +105,9 @@ namespace Plary
 
         #endregion
 
+
         #region Profile Screen (And badge info) functionality
 
-        private void OnFullBadgeClick(object sender, MouseButtonEventArgs e)
-        {
-            this.BadgeInfoScreen.Visibility = Visibility.Visible;
-        }
-
-        private void BadgeInfoFadeOutComplete(object sender, EventArgs e)
-        {
-            this.BadgeInfoScreen.Visibility = Visibility.Hidden;
-        }
-
-        private void OnFullBadgeScreenClick(object sender, MouseButtonEventArgs e)
-        {
-            Storyboard sb = this.FindResource("BadgeInfoFadeOut") as Storyboard;
-            sb.Begin();
-            sb.Completed += BadgeInfoFadeOutComplete;
-        }
 
         #endregion
     }
